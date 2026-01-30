2cfd730abac07854fa7c941920d50a71ba81e398
Seyon <seyontennis@gmail.com>
2026-01-30 11:55:47 +0100

    close #1

--- game.py
+++ game.py
@@ -22,7 +22,7 @@ class Game:
             for c in range(self.columns):
                 self.update(r, c, event)
                 self.draw(r, c, screen)
-        #changed
+        #test
     def update(self, r, c, event):
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.type == 1:
